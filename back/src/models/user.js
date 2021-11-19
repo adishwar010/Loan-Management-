10178ec0aa1d6dfb341644ec69a00e0bc28db30f
backup10 <backupsharma10@gmail.com>
2021-11-19 23:32:27 +0530

    backend updated

--- back/src/models/user.js
+++ back/src/models/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  username: {
+  email: {
     type: String,
     required: true,
   },
--- back/src/routers/userRoutes.js
+++ back/src/routers/userRoutes.js
@@ -16,7 +16,7 @@ userRouter.get("/users", async (req, res) => {
 
 userRouter.post("/signin", async (req, res) => {
   try {
-    const getlogin = await user.findOne({ username: req.body.username });
+    const getlogin = await user.findOne({ email: req.body.email });
     if (getlogin.password == req.body.password) {
     }
     console.log(getlogin);
@@ -24,6 +24,27 @@ userRouter.post("/signin", async (req, res) => {
   } catch (err) {
     res.send(err);
   }
+});
+
+userRouter.post('/login',(req,res)=>{
+  let userData=req.body;
+
+  user.findOne({email:userData.email},(error,user)=>{
+      if(error){
+          console.log(error);
+
+      }
+      else{
+          if(!user){
+              res.status(401).send('Invalid email');
+          }
+          else if(user.password!==userData.password){
+              res.status(401).send('Invalid password');
+          }else{
+              res.status(200).send(user);
+          }
+      }
+  })
 });
 
 module.exports = userRouter;
